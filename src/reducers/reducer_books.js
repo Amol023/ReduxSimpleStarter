55d86ba594076d3d43a3b4ef906b09bce79c0056
Ajosh8 <amol.joshi@nike.com>
2017-07-04 19:56:05 -0700

    feature(reduxBook): MVP redux book catalog

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default () => {
+  return [
+    { title: "JS", pages: 101 },
+    { title: "Tinder", pages: 29 },
+    { title: "Shure", pages: 3 },
+    { title: "Bar", pages: 11 },
+
+  ]
+}
