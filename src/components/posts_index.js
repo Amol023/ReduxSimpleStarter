dab795f408430c05f06eb42ad7614c3aa962aa1f
Ajosh8 <amol.joshi@nike.com>
2017-07-08 15:21:02 -0700

    chore(blog): Hook up connect decorator

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,31 +3,22 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import fetchPosts from '../actions/index';
 
-class PostsIndex extends Component {
+@connect(({ posts }) => ({ posts }), dispatch => bindActionCreators({ fetchPosts }, dispatch))
+export default class PostsIndex extends Component {
   constructor(props) {
     super(props);
-    // this.fetchPosts
   }
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchPosts();
   }
+
   render() {
     const { posts = [] } = this.props;
 
     return (
       <div>
-        {Object.values(posts).map((post, index) =>
-          <div>index</div>
-        )}
+        Hello World!
       </div>
     );
   }
 }
-
-function mapStateToProps({ posts }) { return { posts } };
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchPosts }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,17 +1,11 @@
-import {FETCH_POSTS} from '../actions/index';
+import { FETCH_POSTS } from '../actions/index';
+import { mapKeys } from 'lodash';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_POSTS:
-      return mapResponseToState(action.payload)
+      return mapKeys(action.payload = [], 'id')
     default:
       return state;
   }
-}
-
-const mapResponseToState = (posts = []) => {
-  posts.reduce((state, blog) => {
-    state[blog.id] = blog;
-    return state
-  }, {})
 }
